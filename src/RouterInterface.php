abdf36b4ce67a22d0b09d0bd3b1516485b02d63c
User <y_zuev@mail.ru>
2022-03-26 13:25:16 +0300

    20220325#2

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -4,6 +4,7 @@ namespace Rmphp\Foundation;
 
 use Psr\Http\Message\RequestInterface;
 
+
 interface RouterInterface {
 	public function setStartPoint(string $mountPoint): void;
 	public function withRules(array $rules) : void;
