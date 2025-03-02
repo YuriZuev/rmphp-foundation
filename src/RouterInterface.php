50b52fefeebeb4eab002371e52423e4f23ca4a07
User <y_zuev@mail.ru>
2025-03-02 21:19:08 +0300

    20250302#2

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -5,9 +5,32 @@ namespace Rmphp\Foundation;
 use Psr\Http\Message\RequestInterface;
 
 interface RouterInterface {
+
+	/**
+	 * @param string $mountPoint
+	 */
 	public function setStartPoint(string $mountPoint): void;
+
+	/**
+	 * @param array $rules
+	 */
 	public function withRules(array $rules) : void;
+
+	/**
+	 * @param array $rules
+	 * @return void
+	 */
 	public function withSet(array $rules): void;
+
+	/**
+	 * @param RequestInterface $request
+	 * @return array|null
+	 */
 	public function match(RequestInterface $request) : ?array;
+
+	/**
+	 * @param RequestInterface $request
+	 * @return array|null
+	 */
 	public function matchByArgv(RequestInterface $request) : ?array;
 }
